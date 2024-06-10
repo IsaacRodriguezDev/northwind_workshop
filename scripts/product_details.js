8a51646e26c56a46abf2fe83d765f42e6c2545d5
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-06-10 09:23:44 -0500

    completed view all dropdown functionality

--- scripts/product_details.js
+++ scripts/product_details.js
@@ -1,4 +1,32 @@
 "use strict"
 window.onload = ()=>{
+    let productDetailTableBody = document.querySelector('#productDetailTableBody')
+    productDetailData(productDetailTableBody)
+}
+async function productDetailData(productDetailTableBody){
+    let urlParam = new URLSearchParams(location.search)
+    console.log('not worked')
+    if(urlParam.has('productId')===true ){
+        console.log('worked')
+        productDetail(urlParam.get('productId'),productDetailTableBody)
+    }else{
+        alert('invalid course id')
+        window.location.href = "./product_search.html"
+    }
+}
+async function productDetail(productId,productDetailTableBody){
+    try{
+        let response = await fetch('http://localhost:8081/api/products/'+productId)
+        let productData = await response.json()
+        let newRow = productDetailTableBody.insertRow()
+        let cell1 = newRow.insertCell()
+        cell1.innerHTML = `There are ${productData.unitsInStock} units`
+        let cell2 = newRow.insertCell()
+        cell2.innerHTML = `${productData.supplier}`
+        let cell3 = newRow.insertCell()
+        cell3.innerHTML = `${productData.discontinued}`
+    }catch(err){
+        console.log(err)
+    }
     
 }
--- scripts/product_search.js
+++ scripts/product_search.js
@@ -1,4 +1,47 @@
 "use strict"
 window.onload = ()=>{
+    let tableSearch = document.querySelector('#tableSearch')
+    tableSearch.style.display = 'none'
+    let productSearchDDL = document.querySelector('#productSearchDDL')
+    productSearchDDL.addEventListener('change',showAllData)
     
 }
+async function showAllData(){
+let productSearchDDL = document.querySelector('#productSearchDDL')
+let tableSearch = document.querySelector('#tableSearch')
+
+if(productSearchDDL.value === 'viewAll'){
+    console.log('yes')
+    let tableSearchBody = document.querySelector('#tableSearchBody')
+    let response = await fetch('http://localhost:8081/api/products')
+    let allProducts = await response.json()
+    console.log(allProducts)
+    if(allProducts){
+    tableSearch.style.display = 'block'
+    }
+    for(let i = 0; i< allProducts.length;i++){
+        let newRow = tableSearchBody.insertRow()
+        let cell1 = newRow.insertCell()
+        cell1.innerHTML = allProducts[i].productId
+        let cell2 = newRow.insertCell()
+        cell2.innerHTML = allProducts[i].productName
+        let cell3 = newRow.insertCell()
+        cell3.innerHTML = `$${parseFloat(allProducts[i].unitPrice ).toFixed(2)}`
+        let cell4 = newRow.insertCell()
+        cell4.innerHTML = `<a href="../product_details.html?productId=${allProducts[i].productId}">Details</a>`
+        
+    }
+
+}else{
+        tableSearch.style.display = 'none'
+}
+}
+async function showCategory(){
+    let productSearchDDL = document.querySelector('#productSearchDDL')
+    if(productSearchDDL.value === 'viewAll'){
+        console.log('yes')
+        let response = await fetch('http://localhost:8081/api/products')
+        let allProducts = await response.json()
+    
+    }
+    }
